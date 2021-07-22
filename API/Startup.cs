f04baaea83bf39ee934dacbed8712b6f925b6336
Logoshin Nikolai <lgrf53@gmail.com>
2021-07-22 11:02:27 +0300

    Revert "Added UseCors into startup.cs"
    This reverts commit 9e336187c96ff61d0973e9ce070700d2eb049a6e.

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,7 +34,6 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,8 +53,6 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
